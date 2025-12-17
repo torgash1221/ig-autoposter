539706313d054de5cb0b3f2b8eb1bfe2c9d87aa7
torgash1221 <torgash1221@gmail.com>
2025-12-17 17:50:34 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,4 @@
 from .upload import router as upload_router
 from .publish import router as publish_router
 from .schedule import router as schedule_router
+from .gallery import router as gallery_router
