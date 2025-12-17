7749595b3f97bd4ce3272cb1a0bfe33c04b797ee
torgash1221 <torgash1221@gmail.com>
2025-12-17 02:05:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_CHAT_ID = int(os.getenv("OWNER_CHAT_ID", "0"))
+
+TIMEZONE = "Europe/Kyiv"
 
 BUSINESSES = {
     "ustritso": "🦪 УстриЦО",
