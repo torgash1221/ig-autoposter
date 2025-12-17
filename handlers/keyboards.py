33ba73a3e573b5322bfe333a04c49597e2afbbf6
torgash1221 <torgash1221@gmail.com>
2025-12-17 02:40:19 +0200

    Create keyboards.py

---
+++ handlers/keyboards.py
@@ -0,0 +1,23 @@
+# handlers/keyboards.py
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def publish_keyboard(business: str, content_id: int):
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="📲 Опубликовать",
+                callback_data=f"published:{business}"
+            ),
+            InlineKeyboardButton(
+                text="🔁 Заменить",
+                callback_data=f"replace:{business}"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="🗑 Удалить",
+                callback_data=f"delete:{content_id}"
+            )
+        ]
+    ])
